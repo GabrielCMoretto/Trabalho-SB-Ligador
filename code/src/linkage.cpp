7f811dd83ee2dd9c0485397f52d0ba0b2cbd70e5
GabrielCMoretto <gabrielmorettopvh@yahoo.com.br>
2022-07-20 19:30:54 -0300

    Agora fiz as tabelas com hash

--- code/src/linkage.cpp
+++ code/src/linkage.cpp
@@ -2,9 +2,9 @@
 
 #include <fstream>
 #include <iostream>
-#include <map>
 #include <sstream>
 #include <string>
+#include <unordered_map>
 #include <vector>
 using namespace std;
 
@@ -14,14 +14,18 @@ void linkCodes(string objCode1Name, string objCode2Name) {
   string auxStringNumber;
   int corFactor = 0;
   vector<int> code1Int;
+  vector<int> code2Int;
   int number = 0;
-  map<int, string> useTable1;
-  map<int, string> defTable1;
+  unordered_map<int, string> useTable1;
+  unordered_map<int, string> defTable1;
+  unordered_map<int, string> useTable2;
+  unordered_map<int, string> defTable2;
   int useTableCount = 0;
   int defTableCount = 0;
   vector<int> auxVector;
 
   ifstream objCode1(objCode1Name);
+  ifstream objCode2(objCode2Name);
   if (objCode1.is_open()) {
     while (getline(objCode1, line)) {
       // cout << line << '\n';
@@ -34,11 +38,41 @@ void linkCodes(string objCode1Name, string objCode2Name) {
         } else {
           auxString = line.substr(0, line.find(" "));
           auxStringNumber = line.substr(line.find(" "), line.size());
-          cout << "aha:" << auxStringNumber << "\n";
           stringstream auxStringNumberChar(auxStringNumber);
           while (auxStringNumberChar >> number) {
             auxVector.push_back(number);
           }
+          for (auto& it : auxVector) {
+            useTable1.emplace(it, auxString);
+          }
+          auxString.clear();
+          auxStringNumber.clear();
+          auxStringNumberChar.clear();
+          auxVector.clear();
+          number = 0;
+        }
+      }
+      if (line == "TABELA DEF") {
+        defTableCount++;
+      }
+      if (defTableCount != 0 && line != "TABELA DEF") {
+        if (line == "") {
+          defTableCount = 0;
+        } else {
+          auxString = line.substr(0, line.find(" "));
+          auxStringNumber = line.substr(line.find(" "), line.size());
+          stringstream auxStringNumberChar(auxStringNumber);
+          while (auxStringNumberChar >> number) {
+            auxVector.push_back(number);
+          }
+          for (auto& it : auxVector) {
+            defTable1.emplace(it, auxString);
+          }
+          auxString.clear();
+          auxStringNumber.clear();
+          auxStringNumberChar.clear();
+          auxVector.clear();
+          number = 0;
         }
       }
     }
@@ -48,11 +82,82 @@ void linkCodes(string objCode1Name, string objCode2Name) {
       corFactor++;
     }
     cout << "corFactor:" << corFactor << "\n";
-    // for (auto const &pair : useTable1) {
-    // cout << "{" << pair.first << ": " << pair.second << "}\n";
-    //}
+    for (auto const& pair : useTable1) {
+      cout << "{" << pair.first << ": " << pair.second << "}\n";
+    }
+
+    for (auto const& pair : defTable1) {
+      cout << "{" << pair.first << ": " << pair.second << "}\n";
+    }
     objCode1.close();
 
-  } else
+  } else {
     cout << "Unable to open file";
+  }
+  if (objCode2.is_open()) {
+    while (getline(objCode2, line)) {
+      // cout << line << '\n';
+      if (line == "TABELA USO") {
+        useTableCount++;
+      }
+      if (useTableCount != 0 && line != "TABELA USO") {
+        if (line == "") {
+          useTableCount = 0;
+        } else {
+          auxString = line.substr(0, line.find(" "));
+          auxStringNumber = line.substr(line.find(" "), line.size());
+          stringstream auxStringNumberChar(auxStringNumber);
+          while (auxStringNumberChar >> number) {
+            auxVector.push_back(number);
+          }
+          for (auto& it : auxVector) {
+            useTable2.emplace(it, auxString);
+          }
+          auxString.clear();
+          auxStringNumber.clear();
+          auxStringNumberChar.clear();
+          auxVector.clear();
+          number = 0;
+        }
+      }
+      if (line == "TABELA DEF") {
+        defTableCount++;
+      }
+      if (defTableCount != 0 && line != "TABELA DEF") {
+        if (line == "") {
+          defTableCount = 0;
+        } else {
+          auxString = line.substr(0, line.find(" "));
+          auxStringNumber = line.substr(line.find(" "), line.size());
+          stringstream auxStringNumberChar(auxStringNumber);
+          while (auxStringNumberChar >> number) {
+            auxVector.push_back(number);
+          }
+          for (auto& it : auxVector) {
+            defTable2.emplace(it, auxString);
+          }
+          auxString.clear();
+          auxStringNumber.clear();
+          auxStringNumberChar.clear();
+          auxVector.clear();
+          number = 0;
+        }
+      }
+    }
+    stringstream code2Char(line);
+    while (code2Char >> number) {
+      code2Int.push_back(number);
+    }
+    for (auto const& pair : useTable2) {
+      cout << "{" << pair.first << ": " << pair.second << "}\n";
+    }
+
+    for (auto const& pair : defTable2) {
+      cout << "{" << pair.first << ": " << pair.second << "}\n";
+    }
+    objCode2.close();
+
+  } else {
+    cout << "Unable to open file";
+  }
 }
